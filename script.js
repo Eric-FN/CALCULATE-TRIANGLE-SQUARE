6b17fca5ee659cf6dbffd9c516f2fae149439384
Eric Fonseca <ericfonseca7@gmail.com>
2025-05-13 04:37:37 +0100

    subindo exercicio de calculo com javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+var bQ = prompt("insira o valor da base do quadrado:");
+var hQ = prompt("Insira o valor da altura do quadrado:");
+var formulaQuadrado = bQ * hQ;
+alert(`o resultado da área do quadrado é: ${formulaQuadrado}`);
+
+var bT = prompt("insira o valor da base do traingulo:");
+var hT = prompt("insira o valor da altura do triangulo:");
+var formulaTriangulo = (bT * hT) / 2;
+alert(`o resultado da área do triangulo é: ${formulaTriangulo}`);
